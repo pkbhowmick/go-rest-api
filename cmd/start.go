1fe2a87f64e3496d45065db9825fb68269042f0f
Pulak Kanti Bhowmick <pulak@appscode.com>
2020-12-22 12:31:47 +0600

    Add start command to start the server from cli

---
+++ cmd/start.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"github.com/pkbhowmick/go-rest-api/api"
+	"github.com/spf13/cobra"
+)
+
+func init()  {
+	rootCmd.AddCommand(startCmd)
+}
+
+var startCmd = &cobra.Command{
+	Use: "start",
+	Short: "This command will start the api server",
+	Long: "This command will start the go-rest-api server",
+	Run: func(cmd *cobra.Command, args []string) {
+		api.StartServer()
+	},
+}
